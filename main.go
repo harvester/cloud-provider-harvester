1c9496243c469989521e9765f58cdda255617f50
Jian Wang <jian.wang@suse.com>
2026-09-01 21:13:17 +0200

    Add kubevirt apis to codegen
    Signed-off-by: Jian Wang <jian.wang@suse.com>

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+//go:generate go run pkg/codegen/cleanup/main.go
+//go:generate /bin/rm -rf pkg/generated
+//go:generate go run pkg/codegen/main.go
+//go:generate goimports -w ./pkg/generated
+
 package main
 
 import (
---
+++ pkg/codegen/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	kubevirtv1 "kubevirt.io/api/core/v1"
+
+	controllergen "github.com/rancher/wrangler/v3/pkg/controller-gen"
+	"github.com/rancher/wrangler/v3/pkg/controller-gen/args"
+)
+
+func main() {
+	os.Unsetenv("GOPATH")
+	controllergen.Run(args.Options{
+		OutputPackage: "github.com/harvester/cloud-provider-harvester/pkg/generated",
+		Boilerplate:   "hack/boilerplate.go.txt",
+		Groups: map[string]args.Group{
+			kubevirtv1.SchemeGroupVersion.Group: {
+				Types: []interface{}{
+					kubevirtv1.VirtualMachine{},
+					kubevirtv1.VirtualMachineInstance{},
+				},
+				GenerateTypes:   false,
+				GenerateClients: true,
+			},
+		},
+	})
+}
